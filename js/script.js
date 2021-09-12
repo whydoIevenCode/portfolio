9ebbc82bcd74c713ce103edaaff9138aa23e61d3
Abul Kashem <kasemshishir1100@gmail.com>
2021-09-12 17:03:27 +0600

    about-sec+

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+// const progress = document.querySelectorAll(".progress");
 
+// const num = [70, 60, 50, 10];
+
+// for (let i = 0; i < progress.length; i++) {
+//   progress[i].style.width = num[i] + "%";
+// }
